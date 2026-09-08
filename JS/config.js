03f33c7d2e556c9d35eebec4fd9cf728686dbf03
Diretoria-DPF <dpires292@gmail.com>
2026-09-08 00:25:17 -0300

    Update config.js

--- JS/config.js
+++ JS/config.js
@@ -4,7 +4,7 @@
  */
 const APP_CONFIG = {
   // Substitua pela URL da Web App publicada no Google Apps Script
-  GAS_EXEC_URL: "https://script.google.com/macros/s/AKfycbz_SUA_CHAVE_AQUI_DEPOIS_DO_DEPLOY/exec",
+  GAS_EXEC_URL: "https://script.google.com/macros/s/AKfycbxJ06HP8cUwzBIsXiqR6BC-_RheGhdf5yWKH2jcYN1mm9m5smySdKQ6LIeVs766PV7w/exec",
   SESSION_STORAGE_KEY: "liga_farmacia_auth_session",
   VERSION: "1.0.0",
   TIMEOUT_MS: 15000
