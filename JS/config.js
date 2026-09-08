0bd45ac8f441a368685ca0048b430bc1d91c6336
Diretoria-DPF <dpires292@gmail.com>
2026-09-08 00:12:15 -0300

    Create config.js

---
+++ JS/config.js
@@ -0,0 +1,13 @@
+/**
+ * Configuração Geral da Aplicação Frontend
+ * Liga Acadêmica de Farmácia Universitária
+ */
+const APP_CONFIG = {
+  // Substitua pela URL da Web App publicada no Google Apps Script
+  GAS_EXEC_URL: "https://script.google.com/macros/s/AKfycbz_SUA_CHAVE_AQUI_DEPOIS_DO_DEPLOY/exec",
+  SESSION_STORAGE_KEY: "liga_farmacia_auth_session",
+  VERSION: "1.0.0",
+  TIMEOUT_MS: 15000
+};
+
+Object.freeze(APP_CONFIG);
